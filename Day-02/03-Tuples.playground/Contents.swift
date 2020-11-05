724c94ea0191fdd88250d00f361ad17628100f9c
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 23:08:16 -0600

    Completed: Day 2 of 100 Days Of Swift

---
+++ Day-02/03-Tuples.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+var name = (first: "Taylor", last: "Swift")
+name.0
+name.first
+
+var website = (name: "Apple", url:"www.apple.com")
+website.name
+website.url
+
+var webSite = ["Apple", "www.apple.com"]
+webSite[0]
+webSite[1]
+
+var person = (name: "Paul", age: 40, isMarried: true)
+person.isMarried
+person.age
+person.name
