f91d570f84feec3f2c6b6553bae6914d44a01716
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 20:39:07 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/04-Doubles and Booleans.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+var pi = 3.141
+var myDouble = 1.0
+
+var awesome = true
+
+
+// Examples
+var animated = false
+var dressHasPockets = true
+var isReady = false
+var isEncrypted = true
+var isVisible = false
+var loggedIn = true
