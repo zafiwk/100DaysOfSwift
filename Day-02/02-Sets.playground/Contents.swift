080d47fe26c79f298820287ee60a5450f15b07ee
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 22:26:58 -0600

    Completed: Day 2 of 100 Days of Swift

---
+++ Day-02/02-Sets.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+let colors = Set(["red", "green", "blue"])
+
+let colors2 = Set(["red", "green", "blue", "red", "blue"])
+
+
+// Examples
+var readings = Set([true, false, true, true])
+var attendees = Set([100, 100, 101, 100])
+var names = Set(["Sean", "Paul"])
+var colorss = Set(["Red", "Green", "Red"])
+var ratings = Set([1, 1, 1, 2, 2, 2])
+var scores = Set([9, 10])
