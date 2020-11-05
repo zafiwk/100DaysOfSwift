20511a58d70f3c9a0495b8f3a84d4e0fa8b22979
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 17:26:11 -0600

    Completed: Day 3 of 100 Days Of Swift

---
+++ Day-03/07-The ternary operator.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+let firstCard = 11
+let secondCard = 10
+print(firstCard == secondCard ? "Cards are the same" : "Cards are different")
+
+let isAuthenticated = true
+print(isAuthenticated ? "Welcome!" : "Who are you?")
+
+
+// Examples
+let phone = "iPhone"
+print(phone == "Android" ? "Failure" : "Success")
+
+let numberOfCats = 21
+print(numberOfCats >= 18 ? "Success" : "Failure")
+
+let dexterity = 18
+print(dexterity >= 18 ? "Success" : "Failure")
+
+let isComplete: Bool = false
+print(isComplete == true ? "Failure" : "Success")
+
+let sides = 5
+print(sides <= 6 ? "Success" : "Failure")
