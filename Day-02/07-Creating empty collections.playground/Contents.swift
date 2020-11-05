f957216e1f8b92a526f8c5f5a6300a51c42c6021
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 11:31:23 -0600

    Completed: Day 2 of 100 Days Of Swift

---
+++ Day-02/07-Creating empty collections.playground/Contents.swift
@@ -0,0 +1,28 @@
+import UIKit
+
+// Empty Dictionary
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+// Empty Array
+var results = [Int]()
+
+// Empty Set
+var words = Set<String>()
+var numbers = Set<Int>()
+
+// Empty Arrays and Dictionaries
+var scores = Dictionary<String, Int>()
+var result = Array<Int>()
+
+
+// Examples
+var favoriteColors = Dictionary<String, String>()
+
+var states = Set<String>()
+
+var answers = [Bool]()
+
+var scoores = Array<Int>()
+
+var authorAges = [String: Int]()
