bd5b95bb966bf084cbe9e2626bc1f924412ba83a
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 13:30:49 -0600

    Completed: Day 3 of 100 Days Of Swift

---
+++ Day-03/02-Operator overloading.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// + sums integers like this
+let meaningOfLife = 42
+let doubleMeaning = 42 + 42
+
+// But + also joins strings
+let fakers = "Fakers gonna"
+let action = fakers + "fake"
+
+// You can even use + to join arrays
+let firstHalf = ["John", "Paul"]
+let secondHalf = ["George", "Ringo"]
+let beatles = firstHalf + secondHalf
+
+
+// Examples
+let result = 5.0 * 5.2
+let rresult = 100 + 1
+let reesult = ["Red", "Green"] + ["Blue"]
+let ressult = 100 / 10
+let resuult = "hello" + "!"
+var resullt = 20 % 3
+
