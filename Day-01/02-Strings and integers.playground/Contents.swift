886a56f6af2e270da96dbdbc131e814e257351ec
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 20:25:13 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/02-Strings and integers.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var age = 38
+var population = 8_000_000
+var meaningOfLife = 42
+
+
+// Examples
+var mortgageRemaining = 100_000
+var speed = 88
+var repeatCount = 82
+var highScore = 328_556
+var power = 9001
+var rating = 5
