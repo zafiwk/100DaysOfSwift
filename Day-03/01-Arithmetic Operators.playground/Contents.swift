83940714623426cd0de247da392c2ce70649eb49
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 13:21:44 -0600

    Completed: Day 3 of 100 Days Of Swift

---
+++ Day-03/01-Arithmetic Operators.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+let firstScore = 12
+let secondScore = 4
+
+let total = firstScore + secondScore
+let difference = firstScore - secondScore
+let product = firstScore * secondScore
+let divided = firstScore / secondScore
+
+let remainder = 13 % secondScore // remainders after division
+
+let value: Double = 90000000000000001
+let value2: Int = 90000000000000001
+
+let weeks = 465 / 7
+print("There are \(weeks) weeks until the event")
+
+let weeks2: Double = 465 / 7
+let days = 465 % 7
+print("There are \(weeks) weeks and \(days) days until the event")
+
+let number = 465
+let isMultiple = number.isMultiple(of: 7)
+
+
+// Examples
+var result = 11 % 6
+let resultt = 11 - 6
+var rresult = 7 - 1 - 1
+var reesult = 1 * 5
+let ressult = 20 / 4
+let resullt = 5 + 0
