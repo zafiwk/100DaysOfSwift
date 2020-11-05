a9e0d90b7f34d592277f9fe40cb4d19d8be6814a
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 20:53:48 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/05-String interpolation.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+
+var score = 85
+var str = "Your score was \(score)"
+var results = "The test results are here: \(str)"
+
+var city = "Cardiff"
+var message = "Welcome to \(city)!"
+
+
+// Examples
+var firstName = "Tino", lastName = "López"
+var name = "\(firstName) \(lastName)"
+
+var mesage = "404"
+var alert = "Error: \(mesage)"
+
+var weather = "Rain"
+var forecast = "Today's weather will be \(weather)"
+
+
+let daysRemaning = 5
+var resultado = "\(daysRemaning) days to go"
+
+let years = 10
+let warning = "You need to be \(years) or older"
+
+let namee = "Florentino"
+let greeting = "Hello, \(namee)!"
