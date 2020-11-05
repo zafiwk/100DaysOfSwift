637554618bb425b5939a43c8bc40c37e7007dfd4
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 20:18:11 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/01-Variables.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var str = "Hello, playground"
+str = "Goodbye"
+
+var favoriteShow = "Orange is the New Black"
+favoriteShow = "The Good Place"
+favoriteShow = "Doctor Who"
+
+
+// Examples
+var operatingSystem = "macOS"
+var address = "321 Park Lane"
+var dinosaur = "T-Rex"
+var pirate = "LeChuck"
+var name = "Sophie"
+var strr = "Hello, playground"
