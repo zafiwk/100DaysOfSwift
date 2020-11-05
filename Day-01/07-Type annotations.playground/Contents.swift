55d85d7ed9452db64bf5544e558a75e0937728f8
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 22:07:02 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/07-Type annotations.playground/Contents.swift
@@ -0,0 +1,20 @@
+import UIKit
+
+let str = "Hello, payground"
+
+let album:  String = "Reputation"
+let year: Int = 1989
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+var percentage: Double = 99
+var name: String
+
+
+// Examples
+var average = 32.0
+var sales: Double = 100_000
+var populationMillions = 12.5
+var distance: Double = 320
+var depth = 10.2
+var score: Double = 1000
