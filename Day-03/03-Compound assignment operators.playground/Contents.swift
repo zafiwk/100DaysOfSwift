d4c04804505309c68bc5be529e923adf15cfa7c6
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 13:51:08 -0600

    Completed: Day 3 of 100 Days Of Swift

---
+++ Day-03/03-Compound assignment operators.playground/Contents.swift
@@ -0,0 +1,34 @@
+import UIKit
+
+var score = 95
+score -= 5
+
+var quote = "The rain in Spain falls mainly on the "
+quote += "Spaniards"
+
+var puntos = 1
+puntos += 5
+let puntos1 = puntos + 5
+
+
+// Examples
+var result = 21
+result -= 11
+
+var resultado = 1
+resultado *= 10
+
+var resultado2 = 100
+resultado2 /= 10
+
+var resultado3 = 5
+result *= 2
+
+var resultado4 = 20
+result /= 2
+
+var resultado5 = 10
+resultado5 -= 1
+
+var resultado6 = 10
+resultado6 += 0
