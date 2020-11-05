250414504731898dd0bdf217d22dfa48eb2a179e
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 20:33:30 -0600

    Completed: Day 1 of 100 Days Of Swift

---
+++ Day-01/03-Multi-line strings.playground/Contents.swift
@@ -0,0 +1,56 @@
+import UIKit
+
+var str = """
+Hello, playground
+"""
+
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+var str2 = """
+This goes \
+over multiple \
+lines
+"""
+
+
+// Examples
+var burns = """
+The best laid schemes
+O' mice and men
+Gang aft agley
+"""
+
+var shakespeare = """
+To be or not to be
+that is the question
+"""
+
+var joseph = """
+When I am an old woman,
+I shall wear purple
+with a red hat that doesn't go,
+and doesn't suit me
+"""
+
+var tennyson = """
+Tis better to have loved
+and lost than never
+ to have loved at all
+"""
+
+var wordsworth = """
+I wandered lonely as a cloud
+that floats on high
+o'er vales and hills
+"""
+
+var rossetti = """
+For if the darkness and corruption leave
+A vestige of the thoughts that once I had,
+Better by far you should forget and smile
+Than that you should remember and be sad.
+"""
