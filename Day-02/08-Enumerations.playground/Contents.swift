31a8871f42e20fbc2068f98fd8ed9854440521a3
Florentino Lopez <lopezsf@icloud.com>
2020-11-05 11:38:21 -0600

    Completed: Day 2 of 100 Days Of Swift

---
+++ Day-02/08-Enumerations.playground/Contents.swift
@@ -0,0 +1,22 @@
+import UIKit
+
+enum Result {
+    case success
+    case failure
+}
+let result4 = Result.failure
+
+// Examples
+/*
+ Directions (north, south, etc)
+ 
+ Error types
+ 
+ Genres of movies
+ 
+ Suits in a deck of cards
+ 
+ Months of the year
+ 
+ Days of the week
+*/
