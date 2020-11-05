c79a1908148482f55b52aacee59a316d47e0ebe2
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 23:30:29 -0600

    Completed: Day 2 of 100 Days Of Swift

---
+++ Day-02/05-Dictionaries.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+let heights = ["Taylor Swift": 1.78, "Ed Sheeran": 1.73]
+heights["Taylor Swift"]
+
+
+// Examples
+var roles = ["captain":"Mal", "engineer":"Kaylee"]
+roles["captain"]
+
+var heightss = ["Taylor Swift": 1.78]
+heightss["Taylor Swift"]
+
+var address = ["road": "Park Lane", "city": "Cardiff"]
+address["city"]
+
+var capitals = ["England": "London", "Scotland": "Edinburgh"]
+
+let forecast = ["Monday": "sunny", "Tuesday": "cloudy"]
+
+let scores = ["Sophie": 100]
