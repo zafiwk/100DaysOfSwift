54ebb2ac5bb4abb48f09b9c80ae0594866a61440
Florentino Lopez <lopezsf@icloud.com>
2020-11-04 22:18:12 -0600

    Completed: Day 2 of 100 Days Of Swift

---
+++ Day-02/01-Arrays.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let john = "Jonh Lennon"
+let paul = "Paul McCartney"
+let george = "George Harrison"
+let ringo = "Ringo Starr"
+
+
+let beatles = [john, paul, george, ringo]
+beatles[1]
+
+
+// Examples
+var temperatures = [32.0]
+var avengers = [98.5, 97.1, 99.9]
+var cities: [String] = ["London", "Paris", "New York"]
+var readings: [Bool] = [false, false, true, false]
+var scores: [Int] = [10, 12, 9]
+var singers = ["Taylor", "Adele", "justin"]
