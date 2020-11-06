2959719d7a65d14c661cd8ed07aabf2c851a7ba7
Florentino Lopez <lopezsf@icloud.com>
2020-11-06 10:41:01 -0600

    Completed: Day 5 of 100 Days Of Swift

---
+++ Day-05/01-Writing functions.playground/Contents.swift
@@ -0,0 +1,50 @@
+import UIKit
+
+func printHelp() {
+    let message = """
+Welcome to MyApp!
+
+Run this app inside a directory of images
+MyApp will rezise them all into thumbnails
+"""
+    print(message)
+}
+printHelp()
+
+
+// Examples
+// This code is valid Swift – true or false?
+func listOriginalStarWarsMovies() {
+    for i in 4...6 {
+        print("Episode \(i)")
+    }
+}
+listOriginalStarWarsMovies()
+
+
+func shareToTwitter() {
+    print("Sharing...")
+}
+shareToTwitter()
+
+
+func printWelcome() {
+    print("Hi there!")
+}
+printWelcome()
+
+
+func playMusic() {
+    print("Here's some Ed Sheeran.")
+}
+
+
+func showHelp() {
+    print("Welcome to MyApp.")
+    print("Click the button to start.")
+}
+showHelp()
+
+
+func doNothing() { }
+doNothing()
