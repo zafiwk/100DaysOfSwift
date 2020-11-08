3d9f0472b4dcb98eb3500e13868f249bae4fbdf1
Florentino Lopez <lopezsf@icloud.com>
2020-11-07 19:42:51 -0600

    Completed: Day 6 of 100 Days Of Swift

---
+++ Day-06/01-Creating basic closures.playground/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+let driving = {
+    print("I'm driving in my car")
+}
+driving()
+
+
+// Examples
+// This code is valid Swift – true or false?
+let learnSwift = {
+    print("Closures are like functions")
+}
+learnSwift()
+
+
+let greetUser = {
+    print("Hi there!")
+}
+greetUser()
+
+
+var connectVPN = {
+    print("Connected!")
+}
+connectVPN()
+
+
+let walkDog = {
+    print("Let's go to the park")
+}
+
+
+var castVote = {
+    print("I voted!")
+}
+castVote()
+
+
+var takeMedicine = {
+    print("I feel a little better")
+}
