80b363af9e5692ff3f0db7aa3d9971ec05ad2065
Florentino Lopez <lopezsf@icloud.com>
2020-11-09 20:46:21 -0600

    Completed: Day 9 of 100 Days Of Swift

---
+++ Day-09/05-Access control.playground/Contents.swift
@@ -0,0 +1,76 @@
+import UIKit
+
+struct Person {
+    private var id: String
+    
+    init(id: String) {
+        self.id = id
+    }
+    
+    func identify() -> String {
+        return "My social security number is \(id)"
+    }
+}
+let tino = Person(id: "12345")
+tino.identify()
+
+
+// Examples
+struct Person2 {
+    private var socialSecurityNumber: String
+    init(ssn: String) {
+        socialSecurityNumber = ssn
+    }
+}
+let sarah = Person2(ssn: "555-55-5555")
+
+
+struct Contributor {
+    private var name = "Anonymous"
+}
+let paul = Contributor()
+
+
+struct Office {
+    private var passCode: String
+    var address: String
+    var employees: [String]
+    init(address: String, employees: [String]) {
+        self.address = address
+        self.employees = employees
+        self.passCode = "SEKRIT"
+    }
+}
+let monmouthStreet = Office(address: "30 Monmouth St", employees: ["Paul Hudson"])
+
+
+struct School {
+    var staffNames: [String]
+    private var studentNames: [String]
+    init(staff: String...) {
+        self.staffNames = staff
+        self.studentNames = [String]()
+    }
+}
+let royalHigh = School(staff: "Mrs Hughes")
+
+
+struct Customer {
+    var name: String
+    private var creditCardNumber: Int
+    init(name: String, creditCard: Int) {
+        self.name = name
+        self.creditCardNumber = creditCard
+    }
+}
+let lottie = Customer(name: "Lottie Knights", creditCard: 1234567890)
+
+
+struct App {
+    var name: String
+    private var sales = 0
+    init(name: String) {
+        self.name = name
+    }
+}
+let spotify = App(name: "Spotify")
