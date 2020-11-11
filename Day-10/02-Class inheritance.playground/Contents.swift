a38b03d81b8f8094fb12fd1786470e9131283547
Florentino Lopez <lopezsf@icloud.com>
2020-11-10 18:43:07 -0600

    Completed: Day 10 of 100 Days Of Swift

---
+++ Day-10/02-Class inheritance.playground/Contents.swift
@@ -0,0 +1,112 @@
+import UIKit
+
+// The second difference between classes and structs is that you can create a class based on an existing class – it inherits all the properties and methods of the original class, and can add its own on top.
+
+// "Parent" or "Super" Class
+class Dog {
+    var name: String
+    var breed: String
+    
+    init(name: String, breed: String) {
+        self.name = name
+        self.breed = breed
+    }
+}
+
+// The new class is called the "Child" Class
+class Poodle: Dog {
+    init(name: String) {
+        super.init(name: name, breed: "Poodle")
+    }
+}
+
+
+// Examples
+class Handbag {
+    var price: Int
+    init(price: Int) {
+        self.price = price
+    }
+}
+class DesignerHandbag: Handbag {
+    var brand: String
+    init(brand: String, price: Int) {
+        self.brand = brand
+        super.init(price: price)
+    }
+}
+
+
+class Product {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+class Book: Product {
+    var isbn: String
+    init(name: String, isbn: String) {
+        self.isbn = isbn
+        super.init(name: name)
+    }
+}
+
+
+class Bicycle {
+    var color: String
+    init(color: String) {
+        self.color = color
+    }
+}
+class MountainBike: Bicycle {
+    var tireThickness: Double
+    init(color: String, tireThickness: Double) {
+        self.tireThickness = tireThickness
+        super.init(color: color)
+    }
+}
+
+
+class Instrument {
+    var name: String
+    init(name: String) {
+        self.name = name
+    }
+}
+class Piano: Instrument {
+    var isElectric: Bool
+    init(isElectric: Bool) {
+        self.isElectric = isElectric
+        super.init(name: "Piano")
+    }
+}
+
+
+class Printer {
+    var cost: Int
+    init(cost: Int) {
+        self.cost = cost
+    }
+}
+class LaserPrinter: Printer {
+    var model: String
+    init(model: String, cost: Int) {
+        self.model = model
+        super.init(cost: cost)
+    }
+}
+
+
+class Shape {
+    var sides: Int
+    init(sides: Int) {
+        self.sides = sides
+    }
+}
+class Rectangle: Shape {
+    var color: String
+    init(color: String) {
+        self.color = color
+        super.init(sides: 4)
+    }
+}
