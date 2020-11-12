d604a8fc898ccdbfa463de81386f29e4fe2f30b8
Florentino Lopez <lopezsf@icloud.com>
2020-11-11 23:05:33 -0600

    Completed: Day 12 of 100 Days Of Swift

---
+++ Day-12/01-Handling missing data.playground/Contents.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+var age: Int? = nil
+age = 37
+
+
+// Examples
+// Which of these make good candidates for optionals?
+/*
+ The capital city of a country your user just typed in
+ 
+ Someone's birthday if you know it
+ 
+ The year a user last went on a boat
+ 
+ The current destination of a car's satellite navigation system
+ 
+ Someone's middle name
+ 
+ The position of an item in an array
+ */
