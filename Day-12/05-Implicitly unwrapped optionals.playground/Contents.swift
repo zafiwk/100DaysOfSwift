293f06cd98ded2588fc4bd36932e14f8adaddd92
Florentino Lopez <lopezsf@icloud.com>
2020-11-12 11:58:07 -0600

    Completed: Day 12 of 100 Days Of Swift

---
+++ Day-12/05-Implicitly unwrapped optionals.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+let age: Int! = nil
+
+
+// Examples
+/*
+ Swift uses nil to represent the absence of a value.
+ 
+ A String! does not need to be unwrapped before use.
+ 
+ If you try to use an implicitly unwrapped optional when it is nil, your code crashes.
+ 
+ You can use if let or guard let to unwrap optionals.
+ 
+ A String? must be unwrapped before use.
+ 
+ Optionals allow us to represent the absence of a value.
+ */
