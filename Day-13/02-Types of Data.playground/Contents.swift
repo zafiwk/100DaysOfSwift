4fba18e67934a8d4aa21a8572f84993ab2033853
Florentino Lopez <lopezsf@icloud.com>
2020-11-13 20:35:29 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/02-Types of Data.playground/Contents.swift
@@ -0,0 +1,66 @@
+import UIKit
+
+// Type of Data
+var name: String // String - type annotation
+name = "Soy un String"
+
+
+var age: Int // Int - type annotation
+age = 37
+
+
+var latitude: Double // Double - type annotation
+latitude = 36.166667
+
+
+var longitude: Float // Float - type annotation
+longitude = -86.783333
+longitude = -186.783333
+longitude = -1286.783333
+longitude = -12386.783333
+longitude = -123486.783333
+longitude = -1234586.783333
+
+
+var stayOutTooLate: Bool  // Bool - type annotation
+stayOutTooLate = true
+
+var nothingInBrain: Bool
+nothingInBrain = true
+
+var missABeat: Bool
+missABeat = false
+
+
+// Examples
+// Integers
+var mortgageRemaining = 100_000
+var speed = 88
+var repeatCount = 82
+var highScore = 328_556
+var power = 9001
+var rating = 5
+
+// Strings
+var dogBreed = "samoyed"
+var agee = "23"
+var selectedWood = "mahogany"
+var sizeInInches = "27"
+var currentSong = "Rainbow to the Stars"
+var winner = "Miguel"
+
+// Booleans
+var animated = false
+var dressHasPockets = true
+var isReady = false
+var isEncrypted = true
+var isVisible = false
+var loggedIn = true
+
+// Doubles
+var average = 32.0
+var sales: Double = 100_000
+var populationMillions = 12.5
+var distance: Double = 320
+var depth = 10.2
+var score: Double = 1000
