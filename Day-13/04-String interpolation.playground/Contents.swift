ad2660a6ed33b5318a434239e33fcd4fbcd4b3a2
Florentino Lopez <lopezsf@icloud.com>
2020-11-14 13:43:18 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/04-String interpolation.playground/Contents.swift
@@ -0,0 +1,14 @@
+import UIKit
+
+var name = "Florentino López"
+"Your name is \(name)"
+
+
+var name1 = "Tino Lopez"
+var age = 25
+var latitude = 36.166667
+"Your name is \(name1), your age is \(age), and your latitude is \(latitude)"
+
+
+var age2 = 37
+"You are \(age2) years old. In another \(age2) years you will be \(age2 * 2)."
