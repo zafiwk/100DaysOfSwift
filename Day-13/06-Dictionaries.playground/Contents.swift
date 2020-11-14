e0d6736ba82535c8f2e8fcaaf60ab18db95da297
Florentino Lopez <lopezsf@icloud.com>
2020-11-14 15:35:13 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/06-Dictionaries.playground/Contents.swift
@@ -0,0 +1,17 @@
+import UIKit
+
+var person = ["first": "Taylor", "middle": "Alison", "last": "Swift", "month": "December", "website": "taylorswift.com"]
+person["middle"]
+person["month"]
+
+
+// It might help if I use lots of whitespace to break up the dictionary on your screen, like this:
+var person2 = [
+                "first": "Taylor",
+                "middle": "Alison",
+                "last": "Swift",
+                "month": "December",
+                "website": "taylorswift.com"
+            ]
+person2["middle"]
+person2["month"]
