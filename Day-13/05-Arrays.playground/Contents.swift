5ae1468a673e9626b64d480df82993a47c034535
Florentino Lopez <lopezsf@icloud.com>
2020-11-14 14:12:44 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/05-Arrays.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var evenNumbers = [2, 4, 6, 8]
+
+
+var songs: [String] = ["Shake it Off", "You Belong with Me", "Back to December"]
+songs[0]
+songs[1]
+songs[2]
+type(of: songs)
+
+var canciones: [Any] = ["Shake it Off", "You Belong with Me", "Back to December", 3]
+
+
+// Creating arrays
+var cancion: [String] = []
+
+var canioness = [String]()  // You'll also commonly see this construct:
+
+
+// Array operators
+var sonidos = ["Shake it Off", "You Belong with Me", "Love Story" ]
+var sonidos2 = ["Today was a Fairytale", "Welcome to New York", "Fifteen"]
+var ambos = sonidos + sonidos2
+
+ambos += ["Everything has Changed"] // You can also use += to add and assign
