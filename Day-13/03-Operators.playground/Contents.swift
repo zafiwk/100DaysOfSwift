7b5674d941a76913c3dc18f23f96b36dfa170311
Florentino Lopez <lopezsf@icloud.com>
2020-11-14 10:54:23 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/03-Operators.playground/Contents.swift
@@ -0,0 +1,48 @@
+import UIKit
+
+var a = 10
+a = a + 1 // to add
+a = a - 1 // to subtract
+a = a * a // to multiply
+a = a / 5 // to divide
+var modul = 10 % 3 // to modulus
+
+
+var b = 10
+b += 10
+b -= 10
+
+
+var a1 = 1.1
+var b2 = 2.2
+var c = a1 + b2
+
+
+var name1 = "Florentino Lopez"
+var name2 = "Luis"
+var both = name1 + " and " + name2
+
+
+// Comparison operators
+var x = 1.1
+var y = 2.2
+var z = x + y
+
+z > 3 // greater than
+z >= 3 // greather than or equal
+c > 4
+c < 4 // less than
+
+
+var nombre = "Tino Lopez"
+nombre == "Tino Lopez" // is equal to ==
+
+
+var stayOutTooLate = true
+stayOutTooLate
+!stayOutTooLate   // not operator !
+
+
+var myName = "Luis"
+myName == "Tinillo"
+myName != "Tnillo" // not equal !=
