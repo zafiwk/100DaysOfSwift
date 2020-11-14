dd77d1c9baad6d4d770e205387d58f97b815b21c
Florentino Lopez <lopezsf@icloud.com>
2020-11-13 19:22:30 -0600

    Completed: Day 13 of 100 Days Of Swift

---
+++ Day-13/01-Variables and constants.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+// In Swift, you make a variable using the var keyword, like this:
+var name = "Tino Lopez"
+name = "Luis"
+
+
+// Well, constants use the let keyword rather than var, so you can change your first line of code to say let name rather than var name like this:
+let name2 = "Tinillo Lopez"
+//name2 = "Luis"
+
+
+// Examples
+var operatingSystem = "macOS"
+var address = "321 Park Lane"
+var dinosaur = "T-Rex"
+var pirate = "LeChuck"
+var name3 = "Sophie"
+var str = "Hello, playground"
+
+let width = 14.0
+let username = "twostraws"
+let tweeted = false
+let password = "fr0sti3s"
+let enabled = true
+let age = 26
