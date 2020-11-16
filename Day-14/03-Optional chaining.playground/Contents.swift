0d01b4df47bed4baa63aa7c7f7b1cc2188798c74
Florentino Lopez <lopezsf@icloud.com>
2020-11-16 08:26:36 -0600

    Completed: Day 14 of 100 Days Of Swift

---
+++ Day-14/03-Optional chaining.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+func albumReleased(year: Int) -> String? {
+    switch year {
+    case 2006: return "Taylor Swift"
+    case 2008: return "Fearless"
+    case 2010: return "Speak Now"
+    case 2012: return "Red"
+    case 2014: return "1989"
+    default: return nil
+    }
+}
+let album = albumReleased(year: 2006)?.uppercased()
+print("The album is \(album!)")
+
+
+let str = "Hello World"
+print(str.uppercased())
+
+
+// The nil coalescing operator
+let albun = albumReleased(year: 2006) ?? "unknown"
+print("The album is \(album!)")
+
+
