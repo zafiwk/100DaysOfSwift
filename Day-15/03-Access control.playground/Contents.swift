0e2da7a8ac84210a962ae84b8fd95ae4a9db0b44
Florentino Lopez <lopezsf@icloud.com>
2020-11-16 20:38:32 -0600

    Completed: Day 15 of 100 Days Of Swift

---
+++ Day-15/03-Access control.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+/*
+ -> Public: this means everyone can read and write the property.
+ 
+ -> Internal: this means only your Swift code can read and write the property. If you ship your code as a framework for others to use, they won’t be able to read the property.
+ 
+ -> File Private: this means that only Swift code in the same file as the type can read and write the property.
+ 
+ -> Private: this is the most restrictive option, and means the property is available only inside methods that belong to the type, or its extensions.
+ */
+
+class TaylorFan {
+    private var name: String?
+}
