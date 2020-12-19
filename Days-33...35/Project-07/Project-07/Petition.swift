92c71f553a8ff0f6df8154c57eddd8cdddcc0e0a
Florentino Lopez <lopezsf@icloud.com>
2020-12-18 23:35:20 -0600

    Completed: Day 33...35 of 100 Days Of Swift

---
+++ Days-33...35/Project-07/Project-07/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Petition.swift
+//  Project-07
+//
+//  Created by Florentino Lopez on 18/12/20.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Days-33...35/Project-07/Project-07/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Project-07
+//
+//  Created by Florentino Lopez on 18/12/20.
+//
+
+import Foundation
+
+struct Petitions: Codable {
+    var results: [Petition]
+}
